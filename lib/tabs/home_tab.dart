156d7f4e7628dfe6539acfa46a043a18d899b73b
Bruno Rangel <bmsrangel@hotmail.com>
2019-11-12 21:48:23 -0300

    Ajuste da resposta do GET

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -9,36 +9,30 @@ import 'package:transparent_image/transparent_image.dart';
 import 'package:http/http.dart' as http;
 
 class HomeTab extends StatelessWidget {
-
+  Resp resp;
 
   @override
   Widget build(BuildContext context) {
-
     Future<String> getJSONData() async {
-
       var response = await http.get(
-          Uri.encodeFull(
-              "http://treinamento.educ.ifrn.edu.br/api/educ/diario/meus_diarios/"),
-          headers: {
-            "Authorization": "Token dfba99dbad894452f843d1af00ffbcd559c63e59"
-          });
+          Uri.encodeFull("http://treinamento.educ.ifrn.edu.br/api/educ/diario/meus_diarios/"),
+          headers: {"Authorization": "Token dfba99dbad894452f843d1af00ffbcd559c63e59"});
 
       print('Response status: ${response.statusCode}');
       //print('Response body: ${response.body}');
 
-
-
-
       Map userMap = jsonDecode(response.body);
+      resp = Resp.fromJson(userMap["result"][0]);
+      print(resp.professor.text);
 
-       List<User> users = userMap['result'].map<User>((json)=>
-          User.fromJson(json),
+      //  List<User> users = userMap['result'].map<User>((json)=>
+      //     User.fromJson(json),
 
-       ).toList();
+      //  ).toList();
 
-      print(users.toString());
-
+      // print(users.toString());
     }
+
     getJSONData();
     // Trabalhando o Degrade do app
     Widget _buildBodyBack() => Container(
@@ -58,8 +52,7 @@ class HomeTab extends StatelessWidget {
             SliverAppBar(
               //Onde deixa o item fixo no Scroll
               floating: true,
-              snap:
-                  true, // quando abaixar a tela o titulo vai sumir. Quando subir, ele reaparece.
+              snap: true, // quando abaixar a tela o titulo vai sumir. Quando subir, ele reaparece.
               backgroundColor: Colors.transparent,
               elevation: 0.0,
               flexibleSpace: FlexibleSpaceBar(
@@ -68,7 +61,7 @@ class HomeTab extends StatelessWidget {
               ),
             ),
 
-           /* SingleChildScrollView(
+            /* SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: DataTable(
                 columns: [
@@ -83,7 +76,6 @@ class HomeTab extends StatelessWidget {
                 ],
               ),
             )*/
-
           ],
         )
       ],
