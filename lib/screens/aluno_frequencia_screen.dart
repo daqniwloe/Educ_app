be5ce1bbf83e9635134c038a8bd32df169fa051d
daqniwloe <danil0_braz@hotmail.com>
2019-12-06 13:16:55 -0300

    Aplicativo EDUC para gerenciamento de Professores das escolas da Prefeitura de Canguaretama.

---
+++ lib/screens/aluno_frequencia_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class aluno_frequencia_screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text("Alunos"),
+    centerTitle: true,
+    ),
+    );
+  }
+
+}
